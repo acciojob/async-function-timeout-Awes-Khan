9cfe3b551ad2f12b33d04938ef641c13fefd28ce
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2025-01-09 15:13:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+//your JS code here. If required.;
+
+
+const btn = document.getElementById('btn');
+const output = document.getElementById('output');
+
+btn.addEventListener('click',()=>{
+	makeDelay();
+})
+
+async function makeDelay() {
+  const text = document.getElementById('text').value;
+  const delay = parseInt(document.getElementById('delay').value, 10);
+  
+  new Promise((resolve)=>setTimeout(()=>{
+  	output.innerHTML=text;
+    resolve();
+    
+  },delay));
+	
+}
